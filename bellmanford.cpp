ad3595891a685b741c7703cc95cf9a68f24c5aab
Ruchit Sheth <137379960+ruchitsheth13@users.noreply.github.com>
2024-05-16 21:19:40 +0530

    Update bellmanford.cpp

--- bellmanford.cpp
+++ bellmanford.cpp
@@ -1,4 +1,4 @@
-// O()n*m
+// O(n*m)
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -22,9 +22,9 @@ struct Graph* createGraph(int V, int E) {
 }
 
 void printArr(int dist[], int n) {
-    printf("Vertex   Distance from Source\n");
+    cout << "Vertex   Distance from Source" << endl;
     for (int i = 0; i < n; ++i)
-        printf("%d \t\t %d\n", i, dist[i]);
+        cout << i << " \t\t " << dist[i] << endl;
 }
 
 void BellmanFord(struct Graph* graph, int src) {
@@ -42,21 +42,19 @@ void BellmanFord(struct Graph* graph, int src) {
             int u = graph->edge[j].src;
             int v = graph->edge[j].dest;
             int weight = graph->edge[j].weight;
-            if (dist[u] != INT_MAX
-                && dist[u] + weight < dist[v])
+            if (dist[u] != INT_MAX && dist[u] + weight < dist[v])
                 dist[v] = dist[u] + weight;
         }
     }
-    // Step 3: check for negative-weight cycles.  The above step guarantees shortest distances if graph 
-    // doesn't contain negative weight cycle.  If we get a shorter path, then there is a cycle.
+    // Step 3: check for negative-weight cycles. The above step guarantees shortest distances if graph 
+    // doesn't contain negative weight cycle. If we get a shorter path, then there is a cycle.
     for (int i = 0; i < E; i++) {
         int u = graph->edge[i].src;
         int v = graph->edge[i].dest;
         int weight = graph->edge[i].weight;
-        if (dist[u] != INT_MAX
-            && dist[u] + weight < dist[v]) {
-            printf("Graph contains negative weight cycle");
-            return; // If negative cycle is detected, simply return
+        if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) {
+            cout << "Graph contains negative weight cycle";
+            return; 
         }
     }
     printArr(dist, V);
@@ -68,48 +66,38 @@ int main() {
     int V = 5; 
     int E = 8; 
     struct Graph* graph = createGraph(V, E);
-
-    // add edge 0-1 (or A-B in above figure)
     graph->edge[0].src = 0;
     graph->edge[0].dest = 1;
     graph->edge[0].weight = -1;
 
-    // add edge 0-2 (or A-C in above figure)
     graph->edge[1].src = 0;
     graph->edge[1].dest = 2;
     graph->edge[1].weight = 4;
 
-    // add edge 1-2 (or B-C in above figure)
     graph->edge[2].src = 1;
     graph->edge[2].dest = 2;
     graph->edge[2].weight = 3;
 
-    // add edge 1-3 (or B-D in above figure)
     graph->edge[3].src = 1;
     graph->edge[3].dest = 3;
     graph->edge[3].weight = 2;
 
-    // add edge 1-4 (or B-E in above figure)
     graph->edge[4].src = 1;
     graph->edge[4].dest = 4;
     graph->edge[4].weight = 2;
 
-    // add edge 3-2 (or D-C in above figure)
     graph->edge[5].src = 3;
     graph->edge[5].dest = 2;
     graph->edge[5].weight = 5;
 
-    // add edge 3-1 (or D-B in above figure)
     graph->edge[6].src = 3;
     graph->edge[6].dest = 1;
     graph->edge[6].weight = 1;
 
-    // add edge 4-3 (or E-D in above figure)
     graph->edge[7].src = 4;
     graph->edge[7].dest = 3;
     graph->edge[7].weight = -3;
-    
     BellmanFord(graph, 0);
-
     return 0;
 }
+
